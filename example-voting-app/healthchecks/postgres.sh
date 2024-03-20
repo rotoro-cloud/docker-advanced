cc733129d64556c866ed8eaa297b6406dd5e9081
Rotoro.cloud <72907533+rotoro-cloud@users.noreply.github.com>
2024-03-20 19:46:45 +0300

    Update postgres.sh

--- example-voting-app/healthchecks/postgres.sh
+++ example-voting-app/healthchecks/postgres.sh
@@ -3,8 +3,8 @@ set -eo pipefail
 
 host="$(hostname -i || echo '127.0.0.1')"
 user="${POSTGRES_USER:-postgres}"
-db="${POSTGRES_DB:-$POSTGRES_USER}"
-export PGPASSWORD="${POSTGRES_PASSWORD:-}"
+db="${POSTGRES_DB:-$user}"
+export PGPASSWORD="${POSTGRES_PASSWORD:-$user}"
 
 args=(
 	# force postgres to not use the local unix socket (test "external" connectibility)
